6d9801ea06a2dee09d0c1d500df51cdd14b3b2bc
kimerein <kimerein@users.noreply.github.com>
2022-01-26 18:26:33 -0500

    figures archt spat freq

---
+++ combineTheseNormedToQuiescentControl.m
@@ -0,0 +1,11 @@
+function normedOut_all=combineTheseNormedToQuiescentControl(filenames,whichInFilename,whichForNormVals)
+
+normedOut_all=[];
+for i=1:length(filenames)
+    a=load(whichForNormVals{i});
+    condenom=a.denom;
+    a=load(filenames{i});
+    numer=a.numer(:,whichInFilename(i));
+    normedOut_part=numer./nanmean(condenom,2);
+    normedOut_all=[normedOut_all; normedOut_part];
+end
